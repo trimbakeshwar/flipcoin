9944b0a296ba879149a9f8b507ddfa7a008f5d68
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-28 16:44:57 +0530

    modified:-toss the coin

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,2 +1,14 @@
 echo "welcome to flipcoin"
 
+#toss the coin 
+
+function tossTheCoin(){
+toss=$((RANDOM%2))
+case $toss in 
+0)
+   echo head ;;
+1)
+   echo tail ;;
+esac
+}
+$( tossTheCoin $(()) )
